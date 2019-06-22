5f5c54aa38e796214f281961ffa8c322746edb49
thule <lunthu@gmail.com>
2019-06-22 09:45:53 +0700

    setup policy

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -1,7 +1,29 @@
 class CategoryPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      if user.admin?
+        scope.all
+      end
     end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def edit?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
   end
 end
