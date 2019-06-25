60488928e581e2b3959a7eed07ecbbe4e5fd4cd9
thule <lunthu@gmail.com>
2019-06-25 15:57:35 +0700

    article category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user, foreign_key: "user_id"
   validates :title, presence: true
   validates :content, presence: true
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
